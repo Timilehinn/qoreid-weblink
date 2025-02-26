24f03dcb87e0a363fb1b1953026d062cb7597dd8
timilehin <Makindetimi@gmail.com>
2025-02-26 18:01:12 +0100

    feat: handle query params

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+
+var keys = [
+  'customer_id',
+  'firstname',
+  'lastname',
+  'email',
+  'phone'
+] as const;
+
+type DefaultState = { [K in (typeof keys)[number]]: string };
+
+const defaultState: DefaultState = Object.fromEntries(
+  keys.map(key => [key, ""])
+) as DefaultState;
 
 function App() {
 
+  const params = new URLSearchParams(window.location.search);
+  const [ config, setConfig ] = useState<DefaultState>(defaultState)
+
   useEffect(() => {
+    var data: any = {};
+    keys.map(key => {
+      data[key] = params.get(key);
+    });
+    setConfig(data);
     const script = document.createElement("script");
     script.src = "https://dashboard.qoreid.com/qoreid-sdk/qoreid.js";
     script.async = true;
@@ -17,16 +39,17 @@ function App() {
 
   return (
     <>
+    {JSON.stringify(config)}
       <div
         dangerouslySetInnerHTML={{
           __html: `
-     <qoreid-button
+          <qoreid-button
             id="QoreIDButton"
-            flowId="1142"
-            clientId="5QZ78WT9TUH74NVZY8OL"
+            flowId="${import.meta.env.VITE_FLOW_ID}"
+            clientId="${import.meta.env.VITE_CLIENT_ID}"
             productCode="liveness_nin"
-            customerReference="fsdfsdf"
-            applicantData='{"firstname": "John", "lastname": "Doe", "phone": "08080808080", "email": "jon@nomail.com"}'
+            customerReference="${config.customer_id}"
+            applicantData='{"firstname": "${config.firstname}", "lastname": "${config.lastname}", "phone": "${config.phone}", "email": "${config.email}"}'
           ></qoreid-button>
           `,
         }}
